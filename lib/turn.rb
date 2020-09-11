0b67a924a6245a2cec3603bac068b8763f7b5026
Amelia Elton <ameliareneeelton@gmail.com>
2020-09-11 21:59:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,4 +7,8 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
 end
 
 def valid_move?(board, index)
-  
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  end
+end
+
