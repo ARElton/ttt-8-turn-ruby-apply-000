021755d298d9f97f51a0b0969a59b25f630cbde3
Amelia Elton <ameliareneeelton@gmail.com>
2020-09-11 22:40:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,6 +31,9 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets
-  
+  input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, value = "X")
+  else turn(board)
   
 end
